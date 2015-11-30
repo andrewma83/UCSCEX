7cc603cab28a05d4524d633c3a54147ffbd91ef6
Andrew Ma <andrewma83@hotmail.com>
2015-11-29 17:56:37 -0800

    Initial checkin for chapter 06 assignment.

---
+++ linux_device_driver_assignment/ch_06/main.h
@@ -0,0 +1,32 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#define CDD		"CDD"
+#define CDDNUMDEVS	4	// 2.6
+#define BUF_SZ          4095
+#define LOCAL_BUF_SZ    512
+
+typedef struct _CDD_CONTEXT_T {
+    int count;
+    char *storage;
+    int num_open;
+    spinlock_t sp;
+    struct semaphore mutex_lock;
+} CDD_CONTEXT_T;
+
+typedef struct _CDD_STATS_T {
+    size_t alloc_sz;
+    size_t used_sz;
+    int num_open;
+} CDD_STATS_T;
+
+int proc_file_create (void);
+void proc_file_destroy (void);
+int device_init (void);
+void device_exit (void);
+int ct_init (void);
+void ct_exit (void);
+void CDD_get_stats (CDD_STATS_T *stats);
+extern int buf_type [];
+
+#endif/*__MAIN_H__*/
